3dba13e13d4bbff353fe7dd747969f8e37afaa94
Nicole Franklin <Ncfranklin14@gmail.com>
2022-12-20 11:20:15 -0800

    update icons on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import github from '../assets/github.png'
-import linkedIn from '../assets/linkedin.png'
-import emailIcon from '../assets/email-icon.png'
+import { AiOutlineGithub, AiOutlineMail, AiOutlineLinkedin } from "react-icons/ai";
 
 const Footer = () => (
   <footer className="footer">
-            <address>
-                <a href="mailto:ncfranklin14@gmail.com"><img alt="email logo" src={emailIcon} width='50'/></a>
-                <a href="https://github.com/ncfranklin14" target="_blank"><img alt="Github logo" src={github} width='50'/></a>
-                <a href="https://www.linkedin.com/in/nicole-franklin-8a6001a1/" target="_blank"><img alt="LinkedIn logo" src={linkedIn} width='50'/></a>
-            </address>
+    <div id="contact-links" >
+      <div className="icon-links">
+        <a href="mailto:ncfranklin14@gmail.com" target="_blank"><AiOutlineMail /></a>
+      </div>
+      <div className="icon-links">
+        <a href="https://github.com/ncfranklin14" target="_blank"><AiOutlineGithub /></a>
+      </div>
+      <div className="icon-links">
+        <a href="https://www.linkedin.com/in/nicole-franklin-8a6001a1/" target="_blank"><AiOutlineLinkedin /></a>
+      </div>
+    </div>
   </footer>
 );
+
 
 export default Footer;
